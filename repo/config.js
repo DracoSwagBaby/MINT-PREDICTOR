267309c8edd60ae05f1395f9194dad383417eef0
DracoSwagBaby <worldcheat7@gmail.com>
2026-03-14 00:28:26 +0100

    Create config.js

---
+++ repo/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    VERSION: '4.0',
+    SESSION_TIME: 45 * 60 * 1000, // 45 minutes
+    COOLDOWN_TIME: 12 * 60 * 60 * 1000, // 12 hours
+    ALLOWLIST_URL: 'https://api.npoint.io/311a9ae96ea56dadd341?t=' + Date.now(),
+    UI: {
+        WIDTH: '550px',
+        MAX_HEIGHT: '55vh',
+        COLORS: {
+            PRIMARY: '#48d19d',
+            SECONDARY: '#2a8b6f',
+            DARK: '#0c0c0c',
+            DARKER: '#111111',
+            BORDER: '#1a4d3e'
+        }
+    }
+};
